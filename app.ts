ab2709421e2a3979d15e286611b292de6fc35ddc
Therese Hagwall <90thejag@skola.boras.se>
2023-01-17 14:07:55 +0100

    clean up

--- app.ts
+++ app.ts
@@ -12,21 +12,13 @@ const p = fetch(url)
         cont.removeChild(loading);
         for(let i=0; i < harryPotterObj.length; i++){
             let name = document.createElement("h2") as HTMLElement;
-            let species = document.createElement("p") as HTMLElement;
-            let gender = document.createElement("p") as HTMLElement;
-            let house = document.createElement("p") as HTMLElement;
-            let ancestry = document.createElement("p") as HTMLElement;
-            let actor = document.createElement("p") as HTMLElement;
+            let info = document.createElement("p") as HTMLElement;
             let img = document.createElement("img") as HTMLImageElement;
             let cardInfo = document.createElement("div") as HTMLElement;
             cont.append(cardInfo);
             cardInfo.className = "cardInfo";
             name.className = "Name";
-            species.className = "Info hidden";
-            gender.className = "Info hidden";
-            house.className = "Info hidden";
-            ancestry.className = "Info hidden";
-            actor.className = "Info hidden";
+            info.className = "Info hidden";
             img.className = "Info hidden";
 
             if(harryPotterObj[i].house === "Gryffindor"){
@@ -43,35 +35,22 @@ const p = fetch(url)
             //     cont.removeChild(cardInfo);
             // }
             
-            cardInfo.append(name, species, gender, house, ancestry, actor, img);
+            cardInfo.append(name, info, img);
             
             name.innerHTML = harryPotterObj[i].name;
-            species.innerHTML = `Species: ${harryPotterObj[i].species}`;
-            gender.innerHTML = `Gender: ${harryPotterObj[i].gender}`;
-            house.innerHTML = `House: ${harryPotterObj[i].house}`;
-            ancestry.innerHTML = `Ancestry: ${harryPotterObj[i].ancestry}`;
-            actor.innerHTML = `Actor: ${harryPotterObj[i].actor}`;
+            info.innerHTML = `Species: ${harryPotterObj[i].species} <br /> Gender: ${harryPotterObj[i].gender} <br /> House: ${harryPotterObj[i].house} <br /> Ancestry: ${harryPotterObj[i].ancestry} <br /> Actor: ${harryPotterObj[i].actor}`;
             img.src = harryPotterObj[i].image;
-
             if(harryPotterObj[i].image === ""){
                 cardInfo.removeChild(img);
             }
             
             name.addEventListener("click", function(e){
                 e.preventDefault();
-                if(species.className === "Info"){
-                    species.className = "Info hidden";
-                    gender.className = "Info hidden";
-                    house.className = "Info hidden";
-                    ancestry.className = "Info hidden";
-                    actor.className = "Info hidden";
+                if(info.className === "Info"){
+                    info.className = "Info hidden";
                     img.className = "Info hidden";
                 }else{
-                    species.className = "Info";
-                    gender.className = "Info";
-                    house.className = "Info";
-                    ancestry.className = "Info";
-                    actor.className = "Info";
+                    info.className = "Info";
                     img.className = "Info";
                 }
             })
